5568382848469ad26ea9eb116c2d4b4a1572c1ef
Abhishek V <abhivijay96@gmail.com>
2018-04-08 12:45:54 +0530

    pattern alpha

--- extension.js
+++ extension.js
@@ -8,19 +8,17 @@ function activate(context) {
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "automaton-schema-intellisense" is now active!');
+    // console.log('Congratulations, your extension "automaton-schema-intellisense" is now active!');
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', function () {
-        // The code you place here will be executed every time your command is executed
+    // // The command has been defined in the package.json file
+    // // Now provide the implementation of the command with  registerCommand
+    // // The commandId parameter must match the command field in package.json
+    // let disposable = vscode.commands.registerCommand('extension.sayHello', function () {
+    //     // The code you place here will be executed every time your command is executed
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
-
-    context.subscriptions.push(disposable);
+    //     // Display a message box to the user
+    // });
+    // context.subscriptions.push(disposable);
 }
 exports.activate = activate;
 
